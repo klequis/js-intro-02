cfd3a9194fbb13864fa9933889028e87df5c11a7
klequis <klequis@gmail.com>
2017-09-26 13:59:11 -0700

    added totorials

---
+++ 01.get-element-by-id/script.js
@@ -0,0 +1,6 @@
+main('div2')
+
+function main(whichDiv) {
+  let div = document.getElementById(whichDiv);
+  div.style.backgroundColor = 'red'
+}
--- 03.light-on-off/script.js
+++ 03.light-on-off/script.js
@@ -10,7 +10,7 @@ function toggleLight() {
   let lightbulb = document.getElementById('lightbulb')
   let src = lightbulb.src
   console.log('src', src)
-  if (src === 'file:///home/klequis/dev/wk/light-on-off/images/lightbulb-off.svg') {
+  if (src === 'file:///home/klequis/dev/wk/js-intro-02.complete/03.light-on-off/images/lightbulb-off.svg') {
     lightbulb.src = 'images/lightbulb-on.svg'
   } else {
     lightbulb.src = 'images/lightbulb-off.svg'

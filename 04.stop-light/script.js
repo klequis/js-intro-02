bf9cc81fc9b941e78ac9c32d7dd80af80b384da0
klequis <klequis@gmail.com>
2017-09-26 19:38:28 -0700

    first draft

--- 04.stop-light/script.js
+++ 04.stop-light/script.js
@@ -1,22 +1,28 @@
-changeLight('green')
+main()
+function main() {
+  let ele = document.getElementById("btn-red");
+  ele.addEventListener("click", changeLight, false);
+  ele = document.getElementById("btn-yellow");
+  ele.addEventListener("click", changeLight, false);
+  ele = document.getElementById("btn-green");
+  ele.addEventListener("click", changeLight, false);
+}
 
-function changeLight(color) {
+
+
+function changeLight(e) {
   let top = document.getElementById('top')
   let middle = document.getElementById('middle')
   let bottm = document.getElementById('bottom')
-  console.log('color=', color)
   if (color === 'red') {
-    console.log('color is red')
     top.src = 'images/red-on.svg'
     middle.src = 'images/yellow-off.svg'
     bottom.src = 'images/green-off.svg'
   } else if (color === 'yellow') {
-    console.log('color is yellow')
     top.src = 'images/red-off.svg'
     middle.src = 'images/yellow-on.svg'
     bottom.src = 'images/green-off.svg'
   } else {
-    console.log('color is green')
     top.src = 'images/red-off.svg'
     middle.src = 'images/yellow-off.svg'
     bottom.src = 'images/green-on.svg'
